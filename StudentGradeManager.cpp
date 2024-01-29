df485b63c19b6ceaa634325f95d222b1420e340b
AllesUgo <yangpengxuanab@outlook.com>
2024-01-29 23:24:43 +0800

    程序启动加载配置并显示日志

--- StudentGradeManager.cpp
+++ StudentGradeManager.cpp
@@ -6,18 +6,35 @@
 #include "rbslib/Network.h"
 #include "rbslib/Storage.h"
 #include "module_loader/loader.h"
+#include "log/logger.h"
+#include "configuration_manager/configuration_manager.h"
 using namespace std;
 #include <memory>
 int main()
 {
-	RbsLib::Network::HTTP::HTTPServer server(8081);
+	//读取配置文件
+	Logger::LogInfo("读取配置文件");
+	try
+	{
+		Config::LoadConfig();
+	}
+	catch (std::exception const& ex)
+	{
+		Logger::LogError("读取配置文件失败: %s", ex.what());
+		Logger::LogWarn("使用默认设置启动");
+		Logger::LogInfo("保存默认的配置文件");
+		Config::SaveConfig();
+	}
+	Logger::LogInfo("端口设置为: %d", Config::Port());
+	RbsLib::Network::HTTP::HTTPServer server(Config::Port());
 	Module::ModuleManager module_manager;
-	module_manager.LoadModules(RbsLib::Storage::StorageFile("/home/allesugo/.vs/StudentGradeManager/module_sdk"));
+	Logger::LogInfo("模块路径设置为: %s", Config::ModulePath().c_str());
+	module_manager.LoadModules(RbsLib::Storage::StorageFile(Config::ModulePath()));
 	server.SetGetHandle([](const RbsLib::Network::TCP::TCPConnection& x, RbsLib::Network::HTTP::RequestHeader& h) {
 		cout << h.ToString() << endl;
 		});
 	server.SetPostHandle([&module_manager](const RbsLib::Network::TCP::TCPConnection& x, RbsLib::Network::HTTP::RequestHeader& h,const RbsLib::Buffer&buffer) {
-		std::regex path_reg("^/run/(\\w+).(\\w+)$");
+		std::regex path_reg("^/app/(\\w+).(\\w+)$");
 		std::cmatch m;
 		RbsLib::Network::HTTP::ResponseHeader header;
 		header.status = 404;
