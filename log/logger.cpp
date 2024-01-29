2881ac33c46b6561e79bb5c7e45b7c4c1e887749
AllesUgo <yangpengxuanab@outlook.com>
2024-01-29 23:23:39 +0800

    增加简单日志库

---
+++ log/logger.cpp
@@ -0,0 +1,62 @@
+#include "logger.h"
+#include <time.h>
+#include <cstdarg>
+#include <cstdio>
+void Logger::print_log(int level, const char* log_format, va_list lst)
+{
+	switch (level)
+	{
+	case 3:
+	case 2:
+	case 1:
+	case 0:
+		vprintf(log_format, lst);
+	default:
+		break;
+	}
+}
+void Logger::LogInfo(const char* format, ...)
+{
+	va_list lst;
+	char tmp[80];
+	sprintf(tmp, "[%s] [INFO] ", Time::GetFormattedTime().c_str());
+	va_start(lst, format);
+	Logger::print_log(0, (std::string(tmp) + format + "\n").c_str(), lst);
+	va_end(lst);
+
+}
+
+void Logger::LogWarn(const char* format, ...)
+{
+	va_list lst;
+	char tmp[80];
+	sprintf(tmp, "[%s] [Warn] ", Time::GetFormattedTime().c_str());
+	va_start(lst, format);
+	Logger::print_log(1, (std::string(tmp) + format + "\n").c_str(), lst);
+	va_end(lst);
+}
+
+void Logger::LogError(const char* format, ...)
+{
+	va_list lst;
+	char tmp[80];
+	sprintf(tmp, "[%s] [ERROR] ", Time::GetFormattedTime().c_str());
+	va_start(lst, format);
+	Logger::print_log(2, (std::string(tmp) + format + "\n").c_str(), lst);
+	va_end(lst);
+}
+
+std::string Time::GetFormattedTime(void) noexcept
+{
+	return Time::ConvertTimeStampToFormattedTime(time(0));
+}
+
+std::string Time::ConvertTimeStampToFormattedTime(time_t time_stamp) noexcept
+{
+	char tmp[64];
+	struct tm* timinfo;
+	timinfo = localtime(&time_stamp);
+
+	strftime(tmp, sizeof(tmp), "%Y-%m-%d %H-%M-%S", timinfo);
+	return tmp;
+}
---
+++ log/logger.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+#include <cstdarg>
+class Logger
+{
+private:
+	Logger() = delete;
+	static void print_log(int level, const char* log_format, va_list lst);
+public:
+	static void LogInfo(const char* format, ...);
+	static void LogWarn(const char* format, ...);
+	static void LogError(const char* format, ...);
+};
+class Time
+{
+public:
+	static std::string GetFormattedTime(void)noexcept;
+	static std::string ConvertTimeStampToFormattedTime(time_t time_stamp)noexcept;
+};
